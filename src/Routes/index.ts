33abd885886b19a16aae980e11294f14172953f7
shubhamkunwar10 <fonders@metakul.com>
2025-03-29 18:33:48 +0530

    Feat: Init Routes with db check

---
+++ src/Routes/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import handleError from "../Middleware/handleError.js";
+import { Request, Response, NextFunction } from "express";
+import { MongooseError } from "../DataTypes/enums/Error.js";
+import { isDatabaseHealthy } from "../Utils/db/client.js";
+
+const router = express.Router({ mergeParams: true });
+
+// Middleware to check Mongoose connection
+const CheckDatabaseConnection = async (
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const isDatabseHealthy = await isDatabaseHealthy()
+    if (isDatabseHealthy == false) {
+      // Prisma is not connected
+      throw MongooseError.DatabaseConnectionError();
+    }
+    next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// Define GET API
+router.get("/health", CheckDatabaseConnection, async (_req: Request, res: Response) => {
+    res.status(200).json({ message: "Database is healthy", status: "OK" });
+  });
+
+router.use(handleError)
+
+export default router
