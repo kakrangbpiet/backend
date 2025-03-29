c30d0689b201aef4de8b88c31c479bdc48b32ef1
shubhamkunwar10 <fonders@metakul.com>
2025-03-29 18:18:20 +0530

    Feat: Add swagger

---
+++ src/Utils/swagger.ts
@@ -0,0 +1,62 @@
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation For SAMSARA',
+      version: '1.0.0',
+      description: 'API Information',
+      contact: {
+        name: 'Developer',
+      },
+      servers: [`http://localhost:${PORT}`],
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT', // Optional, specifies the format
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [], // Applies globally to all endpoints
+      },
+    ],
+    servers: [
+      {
+        url: "/v1", // This will automatically prefix /v1 to all paths
+      },
+      {
+        url: "/v1", // This will automatically prefix /v1 to all paths
+      },
+    ],
+  },
+  apis: ['./src/Routes/**/*.ts'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+export const setupSwagger = (app: Express) => {
+  app.get('/swagger.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerDocs);
+  });
+  app.use('/api-docs', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    next();
+  }, swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  
+};
