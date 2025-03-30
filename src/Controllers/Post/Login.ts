e4b77124a4eca335be5517b765adc44827f545d5
shubhamkunwar10 <fonders@metakul.com>
2025-03-30 10:39:23 +0530

    Feat: Add profile

--- src/Controllers/Post/Login.ts
+++ src/Controllers/Post/Login.ts
@@ -502,3 +502,77 @@ export const updatePassword = async (
         return next(error);
     }
 };
+
+
+
+
+
+/*
+/ Get profile 
+*/
+
+
+/**
+ * Get user Profile 
+ * @param email
+ */
+export const profile = async (
+    req: LoginUserRequest,
+    res: Response,
+    next: NextFunction
+) => {
+
+    try {
+        const user = req.user
+
+        console.log(user);
+        
+
+        if (user) {
+            const { email } = user;
+        
+            // First attempt to find user in the 'users' table
+            let userInfo = await prisma.superAdmin.findUnique({
+                where: { email: email },
+                select: {
+                    id: true,
+                    category: true,
+                    email: true,
+                },
+            });
+        
+            // If no user found in 'users' table, search in 'wiwusers' table
+            if (!userInfo) {
+                userInfo = await prisma.samsarauser.findUnique({
+                    where: { email: email },
+                    select: {
+                        id: true,
+                        phoneNumber: true,
+                        category: true,
+                        address: true,
+                        email: true,
+                    },
+                });
+            }
+        
+            // If still no user info found, throw an error
+            if (!userInfo) {
+                throw ErrorEnum.UserNotFoundwithEmail(email);
+            }
+        
+            // Return user info as response
+            return res.status(200).json({
+                message: "Profile Fetched Successfully",
+                data: userInfo,
+                statusCode: 200
+            });
+        }
+        
+        else {
+            throw ErrorEnum.InternalserverError("Error validating User")
+        }
+    } catch (error) {
+        return next(error)
+    }
+};
+
