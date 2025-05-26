a754c48b7952ab52da1d488af751308f8661658a
shubhamkunwar10 <fonders@metakul.com>
2025-05-26 14:31:45 +0530

    Update config

--- src/config.ts
+++ src/config.ts
@@ -48,7 +48,7 @@ export default {
 };
 
 export const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
     region: process.env.AWS_REGION!,
   });
